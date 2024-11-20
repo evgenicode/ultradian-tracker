ff853f3b590432660a08c01abebddd50502cade9
evgenicode <83199154+evgenicode@users.noreply.github.com>
2024-11-20 16:10:57 +0200

    add localtime component

--- src/renderer/src/components/LocalTime.tsx
+++ src/renderer/src/components/LocalTime.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import React  from 'react'
 
 export const LocalTime = (): JSX.Element => {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000)
+
+    return () => clearInterval(timer); //cleanup on unmount
+  }, [])
+
   return (
     <>
       <h1>Local Time component goes here</h1>
+      <div>Current time: {time}</div>
     </>
   )
 }
