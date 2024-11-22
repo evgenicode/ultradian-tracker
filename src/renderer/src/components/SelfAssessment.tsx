eb9ea804fd8a59952f8dfaf061300e13558665c1
evgenicode <83199154+evgenicode@users.noreply.github.com>
2024-11-22 11:44:23 +0200

    fix selfassesment componen

--- src/renderer/src/components/SelfAssessment.tsx
+++ src/renderer/src/components/SelfAssessment.tsx
@@ -1,20 +1,25 @@
 // import React from "react"
 
-const GaugeScale = (scale):any => {
-  
-  return scale
+const GaugeScale = ({scale}: { scale: number }) => {
+  const buttons = Array.from({ length: scale }, (_, i ) => (
+    <button key={i} onClick={ () => handleClick(i) } className="btn">
+      {i + 1}
+    </button>
+  ));
+  return <div>{buttons}</div>
 }
 
-const handleClick = () => {
-  alert("clicked!")
+const handleClick = (i: number) => {
+  console.log(`clicked!${i + 1}`)
 }
+
 
 export const SelfAssesment = () => {
   return (
     <>
       <h1>Energy Flow</h1>
-      <button onClick={handleClick} className="btn">1</button>
-      {/* <GaugeScale scale={1} /> */}
+      
+      <GaugeScale scale={10} />
     </>
   )
 }
